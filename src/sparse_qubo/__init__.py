eb1668916935fcb3c4671e87442c75ccbdfd9bbf
KoheiSuda <sudakohei201@gmail.com>
2026-02-03 12:06:32 +0900

    chore: update shift scheduling problem

--- src/sparse_qubo/__init__.py
+++ src/sparse_qubo/__init__.py
@@ -1,4 +1,7 @@
 from sparse_qubo.core.base_network import NetworkType
 from sparse_qubo.core.constraint import ConstraintType
 
-__all__ = ["ConstraintType", "NetworkType"]
+from .dwave import constraint as create_constraint_dwave
+from .fixstars_amplify import constraint as create_constraint_amplify
+
+__all__ = ["ConstraintType", "NetworkType", "create_constraint_amplify", "create_constraint_dwave"]
